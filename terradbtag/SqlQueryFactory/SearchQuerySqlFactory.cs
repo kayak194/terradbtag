8e2360a2c0d19efa22138d47a9fc942de9df8575
Bastian Kieschnik <Bastian Kieschnik>
2016-12-01 14:39:17 +0100

    SQL Factory refactored.

--- terradbtag/SqlQueryFactory/SearchQuerySqlFactory.cs
+++ terradbtag/SqlQueryFactory/SearchQuerySqlFactory.cs
@@ -6,26 +6,29 @@ namespace terradbtag.SqlQueryFactory
     public static class SearchQuerySqlFactory
     {
         public static string TableName => "FilteredBusinessObjects";
+
         public static string GetSql(ISearchQuery searchQuery)
         {
-            var tagFilter = "";
-            var countConstraint = "";
+            var tagFilter = string.Empty;
             var selectedTagCount = searchQuery.SelectedTags.Count;
             if (selectedTagCount > 0)
             {
-                var filterList = $"'{string.Join("', '", searchQuery.SelectedTags.Select(tag => tag.Content))}'";
-                tagFilter = $"AND content IN ({filterList})";
-                countConstraint = $"HAVING COUNT(id) = {selectedTagCount}";
+                for (var i = 0; i < selectedTagCount; i++)
+                {
+                    tagFilter +=
+                        $"JOIN Tag AS tag{i} ON BusinessObject.id = tag{i}.business_object AND tag{i}.content = '{searchQuery.SelectedTags[i].Content}' ";
+                }
             }
 
             var textFilterSql = "";
             if (!string.IsNullOrEmpty(searchQuery.TextQuery))
             {
-                textFilterSql = $"AND (name LIKE '%{searchQuery.TextQuery}%' OR data LIKE '%{searchQuery.TextQuery}%')";
+                textFilterSql =
+                    $"WHERE (name LIKE '%{searchQuery.TextQuery}%' OR data LIKE '%{searchQuery.TextQuery}%')";
             }
 
             var sql =
-                $"SELECT id, name FROM BusinessObject, Tag WHERE id = business_object {textFilterSql} {tagFilter} GROUP BY id {countConstraint} ORDER BY COUNT(id) DESC";
+                $"SELECT id, name FROM BusinessObject {tagFilter} {textFilterSql}";
 
             var commonTableExpression = $"WITH {TableName}(id, name) AS ({sql})";
 
